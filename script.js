93b00e43543e3090ef30ac2aed0c178f6f414285
Kritika Acharya <acharyakritika44@gmail.com>
2024-02-13 21:22:17 +0545

    list pert

--- script.js
+++ script.js
@@ -2,4 +2,26 @@ const apiKey="oeNej0fRbUpIvwqXXKxsEThiHks1IoCHL65lJIuUlIxw7JXRdNQonA4S";
 const perPage=15;
 let currentPage=1;
 
+const generateHTML = (images) => {
+    images.map(img =>
+        `<li class="card"><img src="images/img1.jpeg" alt="img">
+        <div class="details">
+            <div class="photographer">
+                <i class="uil uil-camera"></i>
+                <span>Kritika Acharya</span>
+            </div>
+            <button><i class="uil uil-import"></i></button>
+        </div>
+    </li>`
+        )
+}
+
+const getImages = (apiURL) => {
+    fetch(apiURL, {
+        headers: { Authorization: apiKey }
+    }).then(res => res.json()).then(data => {
+        generateHTML(data.photos);
+    })
+}
+
 getImages(`https://api.pexels.com/v1/curated?page=${currentPage}per_page=${perPage}`);
