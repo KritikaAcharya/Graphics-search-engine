02f989cdf4aaa88819957a2bb760b23dfdb2468a
Kritika Acharya <acharyakritika44@gmail.com>
2024-02-13 22:07:40 +0545

    lading part done

--- script.js
+++ script.js
@@ -1,27 +1,41 @@
+const imagesWrapper = document.querySelector(".images");
+const loadMoreBtn = document.querySelector(".load-more");
+
 const apiKey="oeNej0fRbUpIvwqXXKxsEThiHks1IoCHL65lJIuUlIxw7JXRdNQonA4S";
 const perPage=15;
 let currentPage=1;
 
 const generateHTML = (images) => {
-    images.map(img =>
-        `<li class="card"><img src="images/img1.jpeg" alt="img">
+    imagesWrapper.innerHTML += images.map(img =>
+        `<li class="card"><img src="${img.src.large2x}" alt="img">
         <div class="details">
             <div class="photographer">
                 <i class="uil uil-camera"></i>
-                <span>Kritika Acharya</span>
+                <span>${img.photographer}</span>
             </div>
             <button><i class="uil uil-import"></i></button>
         </div>
     </li>`
-        )
+        ).join("");
 }
 
 const getImages = (apiURL) => {
+    loadMoreBtn.innerText = "loading...";
+    loadMoreBtn.classList.add("disabled");
     fetch(apiURL, {
         headers: { Authorization: apiKey }
     }).then(res => res.json()).then(data => {
         generateHTML(data.photos);
+        loadMoreBtn.innerText = "Load More";
+        loadMoreBtn.classList.add("disabled");
     })
 }
 
+const loadMoreImages = () => {
+    currentPage++;
+    let apiURL = `https://api.pexels.com/v1/curated?page=${currentPage}per_page=${perPage}`
+    getImages(apiURL);
+}
+
 getImages(`https://api.pexels.com/v1/curated?page=${currentPage}per_page=${perPage}`);
+loadMoreBtn.addEventListener("click",loadMoreImages);
